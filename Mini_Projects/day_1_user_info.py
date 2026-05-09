bb11215487ec2aaaf5229e0aa4820a8011e0d98c
ABDELLATIF EL HATIMY <elhatimyabdu@gmail.com>
2026-05-09 23:01:01 +0100

    Add age validation using if/elif/else conditions
    Added greeting and age-related messages to user input.

--- Mini_Projects/day_1_user_info.py
+++ Mini_Projects/day_1_user_info.py
@@ -1,18 +1,24 @@
 while True:
     try:
+        print("-------------- Hello to My Demo Project Python ----------- ")
+        
         name = input("Enter your name: ")
         age = int(input("Enter your age: "))
         city = input("Enter your city: ")
-
-        print("Hello", name)
-        print("You are", age, "years old")
-        print("You live in", city)
-
+        
+        if age < 12:
+            print("Sorry, you are a child.") # sorry
+        elif age <= 18:
+            print("You are a young man.") # text normal
+        else:
+            print("Hello my brother, you are a man. (Secret text)") # secret text
+            
+        print(f"Hello and welcome Mr {name}. You are {age} years old, and you live in {city}.")
+    
     except ValueError:
         print("Please enter a valid number for age.")
-        continue
+        continue 
 
     again = input("Do you want to continue? Y/N: ")
-
     if again.lower() == "n":
         break
